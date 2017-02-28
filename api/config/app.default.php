18bd5d681442823ed4207ac8c3dcbe6352ef8e04
Duncan Lewis <duncan@piranhr.com>
2017-02-28 16:06:34 +0000

    Updated default app config

--- api/config/app.default.php
+++ api/config/app.default.php
@@ -64,7 +64,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT', '__SALT__'),
+        'salt' => env('SECURITY_SALT', 'C4D83497H7A055xBIT181Bpu1oZFt5KAuDrHV1yl0PTs1CQgHuWRXmAKd23YqzPN'),
     ],
 
     /**
@@ -222,16 +222,16 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => '127.0.0.1',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'root',
+            'password' => 'root',
+            'database' => 'project',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
